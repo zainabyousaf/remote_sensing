965a5d16bdb540b4a70a2bafe0d23621909ca79d
zainabyousaf <zaibhoward@gmail.com>
2020-02-28 10:14:57 +0500

    added new folder

---
+++ programs_for_beginers/armstrong.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int Armstrong_no, fd, ds, sd, td, t;
+cout<<"enter a no.";
+cin>>Armstrong_no;
+td = Armstrong_no%10;
+sd = ((Armstrong_no/10)%10);
+fd = Armstrong_no/100;
+ds=fd*fd*fd+sd*sd*sd+td*td*td;
+if(ds==Armstrong_no){
+    cout<<ds<<" is Armstrong no.";
+}
+else cout << "this is not Armstrong no. because sum of cubes of each digit is not equal to the original no";
+}
+
