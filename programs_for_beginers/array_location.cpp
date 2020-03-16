e17f16d2a4d42a1025578354a812e46a3712f3df
zainabyousaf <zaibhoward@gmail.com>
2020-03-16 11:17:56 +0500

    added new cpp files

---
+++ programs_for_beginers/array_location.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int A[]={8,4,5,7,-4,9,8,-1,-7,-3};
+for(int i=0; i<10; i++){
+if(A[i]==8) cout<<i<<endl;
+}
+//for(int j=0; j<10; j++)
+cout<<A[8];
+}
