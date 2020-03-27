74226f008bb4458e53d57582bb016c1031f5c035
zainabyousaf <zaibhoward@gmail.com>
2020-03-27 14:38:08 +0500

    just added comment

--- programs_for_beginers/Mirror_triangle.cpp
+++ programs_for_beginers/Mirror_triangle.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 int line, no1, no2, space;
+// this is just a simple comment for experiment
 for(line=1; line<=5; line++){
     for(no1=1; no1<=6-line; no1++){cout<<no1;}
     for(space=1; space<= 2*line-3; space++){cout<<"_";}
