bf99449bd8ca22dbe908ab2a13475b17db6b495d
zainabyousaf <zaibhoward@gmail.com>
2020-04-05 03:09:34 +0500

    binar addition operator

--- zaeem_temp/assignment2.cpp
+++ zaeem_temp/assignment2.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
+void sort(int *&, int );
+// array, size
 void make_set(int [], int, int * &,int &);
 // duplicates, size, set, set_size
 void fill_array(int *&,int );
 // array, size
+void Union(int const * const, int const * const, const int, const int, int *&, int &);
+// A, B, Asize,Bsize, C, Csize
+// least privilage provided
+// read it from right to left
+// costant pointer to constant integer
+
 class Set{
 private:
     int size,*set; // first small letter
@@ -34,6 +42,18 @@ public:
             return *this;
         }
     }
+    //------------------
+    const Set operator + (const Set &rhs){
+        int *set_ptr; // its memory will be allocated in union function
+        int set_size;
+        Union(this->set, rhs.set, this->size, rhs.size, set_ptr, set_size);
+        Set s3;
+        s3.size = set_size;
+        s3.set = set_ptr;
+
+        return s3;
+
+    }
 
 
 
@@ -41,15 +61,22 @@ public:
         delete []this->set; // delete previously set memory if any;
         cout << "enter size of set: ";
         cin >> this->size;
+        if(size >0){
         int *temp = new int [this->size];
         fill_array(temp,size);
         make_set(temp,size,this->set,this->size);
+        }
     }
 
     friend ostream &operator << (ostream &out, Set &obj){
         out << "{";
-        for(int i=0; i< obj.size-1; i++) out << obj.set[i] << ",";
-        out << obj.set[obj.size-1] << "}";
+        if(obj.size >0){
+            for(int i=0; i< obj.size-1; i++) out << obj.set[i] << ",";
+            out << obj.set[obj.size-1] << "}";
+        }
+        else{
+            out << "}";
+        }
         return out;
     }
     friend istream &operator >> (istream &in,Set &obj){
@@ -57,6 +84,7 @@ public:
         obj.fill_set();
         return in;
     }
+
 };
 // ==================================== main
 int main(){
@@ -66,10 +94,10 @@ int main(){
     B.fill_set();
     cout << A;
     cout << B;
-    Set C(B);
+    cout << endl;
+    Set C;
+    C=B+A;
     cout << C;
-    A=A;
-    cout << A;
     return 0;
 }
 // =================================== end of main
@@ -98,6 +126,7 @@ void make_set(int elements[], int size, int * &unique_elements,int &new_size){
         for(int i=0; i< index; i++) unique_elements[i] = temp[i];
         // reset the new size
         new_size = index;
+        sort(unique_elements,new_size);
         delete []temp; // delete temp;
         // temp is local var, no need to fix dangling
         // it will die itself
@@ -106,4 +135,65 @@ void make_set(int elements[], int size, int * &unique_elements,int &new_size){
 void fill_array(int *&array,int size){
     cout << "enter elements space separated: ";
     for(int i=0; i< size; i++) cin >> array[i];
+}
+
+//-----------------
+void sort(int *&arr, int size){
+    //bubble sort
+    for(int i=0; i< size-1; i++){
+        bool swap=false;
+        for(int j=0; j< (size-i-1);j++){
+            if( *(arr+j) > *(arr+j+1)){
+                int temp=*(arr+j);
+                *(arr+j) = *(arr+j+1);
+                *(arr+j+1) = temp;
+                swap = true;
+            }
+        }
+        if(! swap) break;
+    }
+}
+//-------------------
+void Union(int const * const A, int const * const B, const int a_size, const int b_size, int *&C, int &size){
+    // copy unique elements into C
+    int c_index=0;
+    int *u = new int [a_size+b_size]; // max possible size
+    // it will be shrunk if needed at the end of function
+    bool found;
+    //----------------------copying array a into c
+    for(int i=0; i< a_size; i++){
+        found=false;
+        //set contains only one occurance
+        for(int j=0; j< c_index ; j++){
+            if ( *(u+j) == *(A+i)) {
+                found=true;break;
+            }
+        }
+        if (! found) {
+            // if it is unique element then insert
+            //u[c_index++]=A[i];
+            *(u+c_index++) = *(A+i);
+        }
+    }
+    //----------------- copying array b
+    for(int i=0; i< b_size; i++){
+            found=false;
+        for(int j=0; j< c_index ; j++){
+            if ( *(u+j) == *(B+i)) {
+                found=true;break;
+            }
+        }
+        if (! found) {
+            //u[c_index++]=B[i];
+            *(u+c_index++) = *(B+i);
+        }
+    }
+    size=c_index;
+
+    int *final_arr = new int [size]; // shrinking array
+    for(int i=0; i< size; i++) *(final_arr+i) = *(u+i);
+
+    sort(final_arr,size);
+    C=final_arr;
+    delete u; //no need to NULL the ptr because it will die itself
 }
