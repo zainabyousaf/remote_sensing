6fb3812246b6c4e745d8e1ff3337567ab700b0ee
zainabyousaf <zaibhoward@gmail.com>
2020-04-07 20:31:13 +0500

    hi

--- zaeem_temp/class_activity/drivers.cpp
+++ zaeem_temp/class_activity/drivers.cpp
@@ -1,73 +1,161 @@
-
+// Aggregation and composition
+// l19-1196 CS-2E1
+// zaeem Yousaf
+// class point
+//all_classes.cpp
+#include "all_classes.h"
 #include <iostream>
-using namespace std;
-class Student{
-private:
+//---------------------- Exercise 1
 
-    char name[50];
-    char rollNo[8];
-    float cgpa;
-public:
-Student(const char myroll[], const char *myname, float cg){
-      cgpa = cg;
-    for(int i=0; i< 7; i++) rollNo[i]=myroll[i];
+Point::Point(int a=0,int b=0){
+  x=a;
+  y=b;
+  std::cout << "Point() called\n";
+}
+void Point::print(){
+  std::cout << "(" << x << "," << y << ")";
+}
+
+Point::~Point(){
+  std::cout << "~Point() called\n";
+}
+
+//------------------------ Exercise 2
+
+Circle::Circle(){
+
+  std::cout << "Circle() called\n";
+}
+Circle::Circle(int a, int b, float c): center(a,b) {
+  radius = c;
+}
+
+void Circle::print(){
+  center.print();
+  std::cout  << " and radius: " << radius << std::endl;
+}
+
+Circle::~Circle(){
+  std::cout << "~Circle() Called\n";
+}
+
+
+//---------------------------- CD class Exercise 3
+
+CompactDisc::CompactDisc(const char *t,int c){
+  int index=0;
+  while(t[index] != '\0') index++;
+  for(int i=0; i< index; i++) title[i]=t[i];
+  title[index]='\0';
+  capacity = c;
+  std::cout << "compactDics() called\n";
+}
+//-------------------- getter title
+char* CompactDisc::get_title(){
+  return this->title;
+}
+//-------------------- setter title
+void CompactDisc::set_title(const char *t){
+  int index=0;
+  while(t[index] != '\0') index++;
+  for(int i=0; i< index; i++) title[i]=t[i];
+  title[index]='\0';
+}
+//---------------------- getter for capacity
+int CompactDisc::get_capacity(){
+  return capacity;
+}
+//---------------------- setter for capacity
+void CompactDisc::set_capacity(int c){
+  capacity = c;
+}
+
+CompactDisc::~CompactDisc(){
+  std::cout << "~CompactDisc() called\n";
+}
+
+
+//--------------------- Exercise 4
+
+CDDrive::CDDrive(const char *m, int s){
+  aCompactDisc = 0;
+  speed = s;
+  int index=0;
+  while(m[index]) manufacturer[index] = m[index++];
+  manufacturer[index]='\0';
+  std::cout << "CDDrive() called\n";
+}
+void CDDrive::InsertCD(CompactDisc *c){
+  aCompactDisc = c;
+}
+void CDDrive::Play (){
+  if(aCompactDisc != 0){
+    std::cout << "title: " << aCompactDisc->get_title() << std::endl;
+    std::cout << "Capacity: " << (aCompactDisc->get_capacity()) << std::endl;
+  }
+  std::cout << "please, Insert Disk.\n";
+
+}
+//-------------------------
+CompactDisc * CDDrive::EjectCD(){
+  return aCompactDisc;
+}
+
+//CDDrive::~CDDrive(){
+//  std::cout << "~CDDrive() called\n";
+//}
+//-----------------------------------
+
+Student::Student(const char r[7], const char *n, float cg){
+  for(int i=0; i< 7; i++) rollNo[i]=r[i];
   rollNo[7]='\0';
-int ind=0;
-  while(myname[ind] != '\0')ind++;
-  for(int i=0; i< ind; i++) name[i]=myname[i];
-name[ind]='\0';
+  int index=0;
+  while(n[index] != '\0')index++;
+  for(int i=0; i< index; i++) name[i]=n[i];
+  name[index]='\0';
+  cgpa = cg;
+}
+void Student::print(){
+  std::cout << "rollNo: " << rollNo << " Name: " << name << " cgpa: " << cgpa << std::endl;
 
 }
-void print(){
-  cout << "rollNo: " << rollNo << " Name: " << name << " cgpa: " << cgpa << endl;
-
-}
-char * GetName(){
+char * Student::GetName(){
   return name;
 }
-float GetCGPA(){
+float Student::get_cgpa(){
   return cgpa;
 }
-};
 
-
-
-using namespace std;
-class Society{
-    private:
-    char name[50];
-    Student* president;
-    Student *members[5];
-    int noOfStudents;
-
-Society(char *myname){
-  int ind=0;
+//-----------------------------
+Society::Society(char *n){
+  int index=0;
   noOfStudents =0;
-  while(myname[ind] != '\0') ind++;
-  for(int i=0; i< ind; i++) name[i] = myname[i];
-  name[ind]='\0';
+  while(n[index++] != '\0');
+  for(int i=0; i< index; i++) name[i] = n[i];
+  name[index]='\0';
 
+  //setting to null
   president=0;
   for(int i=0; i< 5; i++) members[i]=0;
 
 }
-
-void AppointPresident(Student &stu){
-  if(president==0 && (stu.GetCGPA()) > 3.0){
-    president = &stu;
+//--------------------
+void Society::AppointPresident(Student &s){
+  if(president==0 && (s.get_cgpa()) > 3.0){
+    president = &s;
   }
-  else if(president !=0 && stu.GetCGPA() >3){
-    cout << stu.GetName() << " can not be appointed as president,President is not vacant\n";
+  else if(president !=0 && s.get_cgpa() >3){
+    std::cout << s.GetName() << " can not be appointed as president,President is not vacant\n";
   }
   else{
-    cout << stu.GetName() << " cannot be appointed as president,gpa critaria is not met\n";
+    std::cout << s.GetName() << " cannot be appointed as president,gpa critaria is not met\n";
 
   }
 }
-
-void AddMember(Student &s){
+//-----------------------
+void Society::AddMember(Student &s){
   if(president==&s){
-    cout << s.GetName() << " cannot be added, he is president\n";
+    std::cout << s.GetName() << " cannot be added, he is president\n";
   }
   else{
     bool found =false;
@@ -75,10 +163,10 @@ void AddMember(Student &s){
       if(&s==members[i]) {found=true;break;}
     }
     if(found){
-      cout << s.GetName() << " cannot be added as he is already memeber.\n";
+      std::cout << s.GetName() << " cannot be added as he is already memeber.\n";
     }
     else if( noOfStudents >=5){
-      cout << s.GetName() << " cannot be added because there is not vacant.\n";
+      std::cout << s.GetName() << " cannot be added because there is not vacant.\n";
     }
     else{
       members[noOfStudents++] = &s;
@@ -86,107 +174,24 @@ void AddMember(Student &s){
   }
 }
 
-void PrintInfo(){
-  cout << "Society Name: " << name << std::endl << std::endl;
-  cout << "President: ";
+void Society::PrintInfo(){
+  std::cout << "Society Name: " << name << std::endl << std::endl;
+  std::cout << "President: ";
   if(president != 0)
-    cout << (president->GetName()) << std::endl;
-  else{ cout << " Not Available.\n";}
+    std::cout << (president->GetName()) << std::endl;
+  else{ std::cout << " Not Available.\n";}
   for(int i=0; i< 5; i++){
-    cout << "Member " << i+1 <<": ";
+    std::cout << "Member " << i+1 <<": ";
     if(i < noOfStudents){
-      cout << members[i]->GetName() << std::endl;
+      std::cout << members[i]->GetName() << std::endl;
     }
     else{
-      cout << "Not available.\n" << std::endl;
+      std::cout << "Not available.\n" << std::endl;
     }
   }
 }
 
-~Society(){
+Society::~Society(){
 
 }
-
-};
-
-class Point{
-    private:
-    int x,y;
-    public:
-Point(int a=0,int b=0){
-  x=a;
-  y=b;
-
-}
-void print(){
-  cout << "(" << x << "," << y << ")";
-}
-
-~Point(){
-  cout << "~Point() called\n";
-}
-};
-
-class Circle{
-    private:
-    float radius;
-    Point center;
-public:
-Circle(){
-
-
-}
-Circle(int a, int b, float c): center(a,b) {
-  radius = c;
-}
-
-void print(){
-  center.print();
-  cout  << " and radius: " << radius << endl;
-}
-
-~Circle(){
-
-}
-};
-
-
-class CompactDisc{
-    private:
-    char title[20];
-    int capacity;
-public:
-CompactDisc(const char *t,int c){
-  int ind=0;
-  while(t[ind] != '\0') ind++;
-  for(int i=0; i< ind; i++) title[i]=t[i];
-  title[ind]='\0';
-  capacity = c;
-
-}
-
-char* GetTitle(){
-  return this->title;
-}
-
-void SetTitle(const char *t){
-  int ind=0;
-  while(t[ind] != '\0') ind++;
-  for(int i=0; i< ind; i++) title[i]=t[i];
-  title[ind]='\0';
-}
-
-int get_capacity(){
-  return capacity;
-}
-
-void set_capacity(int c){
-  capacity = c;
-}
-
-~CompactDisc(){
-
-}
-};
-
 
